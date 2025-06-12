4b0d685f3b274998b080ee31a02581be9677f6f2
Neha Thul <92106094+NehaThul@users.noreply.github.com>
2025-06-12 20:58:09 +0530

    Create fs.js

---
+++ fs.js
@@ -0,0 +1,31 @@
+const { log } = require('console')
+const fs = require('fs')
+const http = require('http')
+
+http.createServer((req, res) => {
+     // fs.readFile('index.html',(err, data)=>{
+     //      res.writeHead(200, {'Content-Type': 'text/html',})
+     //      res.write(data)
+     //      res.end()
+
+     // })
+     console.log("server is started")
+     // fs.unlink('inde.html',(err)=>{
+     //      if(err) throw err
+     //      console.log('file is updated');
+     // })
+
+     fs.open('index.js', 'w', (err) => {
+          fs.readFile('index.js', (err, data) => {
+               res.writeHead(200, { 'Content-Type': 'text/plain' })
+               res.write(data)
+               res.end()
+               
+               if (err) throw err
+               console.log(err);
+
+          })
+          
+     })
+})
+     .listen(5000)
