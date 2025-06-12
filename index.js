f4fb0b428ced3d0e230cc4dcc2237e9357ecb46f
Neha Thul <92106094+NehaThul@users.noreply.github.com>
2025-06-12 20:59:27 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,36 @@
+// const http = require('http')
+// http.createServer((req, res)=>{
+//     res.writeHead(200, {'Content-Type': 'text/plain',})
+//     res.write(req.url)
+//   res.end()
+//   console.log("server started");
+
+// })
+// .listen(5000)
+
+// const { log } = require('console')
+// const url = require('url')
+// var adr = 'https://in.bookmyshow.com/explore/home/amravati'
+// var q = url.parse(adr, true)
+// console.log(q.host);
+// console.log(q.hostname);
+// console.log(q.href);
+// console.log(q.path);
+// console.log(q.port);
+// console.log(q.protocol);
+
+// document.write("hellow world")
+// console.log("hello")
+
+// console.log(__dirname)
+// console.log(__filename) 
+
+// setTimeout(functiom cb, ms)
+// printhello()
+// function printhello(){
+//   console.log("hello")
+// }
+
+// var t = setTimeout(printhello,3000);
+// clearTimeout(t);
+
